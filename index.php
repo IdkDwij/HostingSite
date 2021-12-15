077192e163876781180f47849fb025aaa54f80fc
Dwij <94942567+IdkDwij@users.noreply.github.com>
2021-12-15 14:55:13 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+	require('anti_ddos/start.php');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/main.css">
+		<title>Games frfr</title>
+	</head>
+	<body>
+	<a href="supersmashflash">Super Smash Flash</a>
+	</body>
+</html>
