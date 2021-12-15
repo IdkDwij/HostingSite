dda7bdc88a1de84f293b62a75b6b75ffeea1a1f0
Dwij <94942567+IdkDwij@users.noreply.github.com>
2021-12-15 14:56:52 -0500

    Create index.php

---
+++ supersmashflash/index.php
@@ -0,0 +1,16 @@
+<?php 
+	require('anti_ddos/start.php');
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Super Smash Flash frfr</title>
+	</head>
+<body>
+
+<object width="100%" height="auto"></object>
+<embed src="Super-Smash-Flash.swf" width="100%" height="auto"></embed>
+
+</body>
+</html>
